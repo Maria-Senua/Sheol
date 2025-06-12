0bfa854b91ba9ac38de33d3d5fd2a95c3e036efa
Maria Smirnova <92306135+Maria-Senua@users.noreply.github.com>
2025-06-12 11:20:03 +0200

    add sounds to folder

---
+++ Assets/TestEnvironment/Scripts/MusicController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public AudioClip[] musicList;
+
+    private void Awake()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
