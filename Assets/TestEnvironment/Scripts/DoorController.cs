ad1daa7e953b05fb7a06cd63b7d36be327e9e324
Maria Smirnova <92306135+Maria-Senua@users.noreply.github.com>
2025-07-08 17:55:29 +0200

    test door anim

--- Assets/TestEnvironment/Scripts/DoorController.cs
+++ Assets/TestEnvironment/Scripts/DoorController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class DoorController : MonoBehaviour
@@ -9,12 +10,23 @@ public class DoorController : MonoBehaviour
         OPEN
     }
 
-    private DoorStates currentState = DoorStates.LOCKED;
+    private DoorStates currentState = DoorStates.CLOSED;
+
+    [SerializeField] private float openAngle = 90f;
+    [SerializeField] private float animationDuration = 1f;
+    [SerializeField] private bool hingeOnLeft = true;
+
+    private bool isAnimating = false;
+
+    private Quaternion closedRotation;
+    private float doorWidth;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        currentState = DoorStates.LOCKED;
+        currentState = DoorStates.CLOSED;
+        closedRotation = transform.rotation;
+        doorWidth = transform.localScale.x;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -27,20 +39,55 @@ public class DoorController : MonoBehaviour
 
     public void TouchDoor()
     {
-        if (currentState == DoorStates.CLOSED)
+        if (isAnimating) return;
+
+        switch (currentState)
         {
-            currentState = DoorStates.OPEN;
-            //add anim
-        } else if (currentState == DoorStates.OPEN)
-        {
-            currentState = DoorStates.CLOSED;
-            //add anim
+            case DoorStates.LOCKED:
+                Debug.Log("DoorState is locked.");
+                break;
+            case DoorStates.CLOSED:
+                StartCoroutine(RotateDoor(true));
+                currentState = DoorStates.OPEN;
+                Debug.Log("DoorState is opening.");
+                break;
+            case DoorStates.OPEN:
+                StartCoroutine(RotateDoor(false));
+                currentState = DoorStates.CLOSED;
+                Debug.Log("DoorState is closing.");
+                break;
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator RotateDoor(bool opening)
     {
-        
+        isAnimating = true;
+
+        // Determine hinge position
+        Vector3 hingeOffset = (hingeOnLeft ? -transform.right : transform.right) * (doorWidth / 2f);
+        Vector3 hingePosition = transform.position + hingeOffset;
+
+        float elapsed = 0f;
+        float totalAngle = opening ? openAngle : -openAngle;
+
+        while (elapsed < animationDuration)
+        {
+            float deltaAngle = (totalAngle / animationDuration) * Time.deltaTime;
+            transform.RotateAround(hingePosition, Vector3.up, deltaAngle);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        isAnimating = false;
     }
+
+    void OnDrawGizmosSelected()
+    {
+        Vector3 hingeOffset = (hingeOnLeft ? -transform.right : transform.right) * (transform.localScale.x / 2f);
+        Vector3 hingePosition = transform.position + hingeOffset;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(hingePosition, 0.02f);
+    }
+
 }
