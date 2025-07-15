f08ace9c23175029a7820ba63de36f1f36980eea
Maria Smirnova <92306135+Maria-Senua@users.noreply.github.com>
2025-07-16 00:42:14 +0200

    add new start scene

---
+++ Assets/TestEnvironment/Scripts/BoatMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMovement : MonoBehaviour
+{
+    public Transform newBoatPos;
+    public float drawningTime = 12f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(MoveToPosition(newBoatPos.position, drawningTime));
+
+
+
+    }
+
+    private IEnumerator MoveToPosition(Vector3 target, float duration)
+    {
+        Vector3 startPos = transform.position;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float t = elapsed / duration;
+            transform.position = Vector3.Lerp(startPos, target, t);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = target; 
+    }
+
+}
