037df7a49701084e865c6090011f4c9d8e53a69d
Maria Smirnova <92306135+Maria-Senua@users.noreply.github.com>
2025-07-15 14:25:33 +0200

    made diary grabbable and flip page

--- Assets/TestEnvironment/Scripts/Diary.cs
+++ Assets/TestEnvironment/Scripts/Diary.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Diary : MonoBehaviour
 {
     private Animator animator;
+    public GameObject pageFlipper;
+
+    public UnityEvent onPageFlipped;
+    public UnityEvent onPageHidden;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animator = gameObject.GetComponent<Animator>();
         animator.enabled = false;
+        pageFlipper.SetActive(false);
+        onPageHidden?.Invoke();
     }
 
     // Update is called once per frame
@@ -20,5 +27,13 @@ public class Diary : MonoBehaviour
     public void OpenDiary()
     {
         animator.enabled = true;
+        pageFlipper.SetActive(true);
+    }
+
+    public void FlipPage()
+    {
+        animator.SetTrigger("flip");
+        pageFlipper.SetActive(false);
+        onPageFlipped?.Invoke();
     }
 }
