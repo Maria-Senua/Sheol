68eabe6eadfbb67501cd2a7f556a53fdc4fd1116
Maria Smirnova <92306135+Maria-Senua@users.noreply.github.com>
2026-05-09 11:12:38 +0200

    fadeout when in collsision with room view blocker

--- Assets/Scripts/ClosedVoiceTrigger.cs
+++ Assets/Scripts/ClosedVoiceTrigger.cs
@@ -3,22 +3,37 @@ using UnityEngine;
 
 public class ClosedVoiceTrigger : MonoBehaviour
 {
-    private AudioSource audioSource;
+    //private AudioSource audioSource;
+    private MeshRenderer meshRenderer;
+    [SerializeField] private FadeScreen fadeScreen;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        audioSource = GetComponent<AudioSource>();
+        //audioSource = GetComponent<AudioSource>();
+        //meshRenderer = GetComponent<MeshRenderer>();
+        //meshRenderer.enabled = false;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            audioSource.Play();
+            //audioSource.Play();
+            //meshRenderer.enabled = true;
+            fadeScreen.FadeOut();
         }
             
         
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //meshRenderer.enabled = false;
+            fadeScreen.FadeIn();
+        }
     }
 
     // Update is called once per frame
